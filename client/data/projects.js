417138c4356db4e27d037a33271b81b0ab0b7fb4
BearFox <nick.mjac@gmail.com>
2016-07-04 18:11:35 +1200

    create a data file for project data

---
+++ client/data/projects.js
@@ -0,0 +1,50 @@
+const projects = [
+  {
+    id: 'pitch-in',
+    title: 'pitch in',
+    tech: 'React, Redux, Express, Sass, PostGres',
+    info: [
+      {heading: "Summary",
+       text: 'An app for increasing volunteer engagement with micro-volunteer opportunites.'},
+      {heading: "Development Process",
+       text: ''},
+      {heading: "Technologies",
+       text: ''}
+    ]
+  },
+  {
+    id: 'ww',
+    title: "word's worth",
+    tech: "Express, socket.io, CSS, jQuery, JavaScript",
+    info: [
+      {heading: "Summary",
+       text: 'This is a project I started at EDA. It is a game to encourage thinking about words and semantics, using a semantic similiarity comparison.'},
+      {heading: "Development Process",
+       text: ''},
+      {heading: "Technologies",
+       text: ''}
+    ]
+  },
+  {
+    id: 'eda',
+    title: 'Enspiral Dev Academy',
+    tech: 'So many technologies!',
+    info: [
+      {heading: "Summary",
+       text: 'EDA runs an intensive bootcamp training for web development.'}
+    ]
+  },
+  {
+    id: 'njco',
+    title: 'nickjacobs.co',
+    tech: 'React, Redux, Sass, Express',
+    info: [
+      {heading: "Summary",
+       text: 'This project was to create the site you are currently on.'},
+      {heading: "Technologies",
+       text: ''}
+    ]
+  }
+]
+
+export default projects
