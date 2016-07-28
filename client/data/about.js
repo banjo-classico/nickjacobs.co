4883bf81e943882eddb3a5410332cd4a2d199bed
BearFox <nick.mjac@gmail.com>
2016-07-28 13:56:35 +1200

    update project data and add pngs

---
+++ client/data/about.js
@@ -0,0 +1,3 @@
+export const intro = 'I am a full stack web developer who graduated from Enspiral Dev Academy in Wellington, NZ. I love making things, and my mission is to create beautiful technology that improves people\'s lives.'
+export const past = 'In a previous life, I was a fully qualified speech language therapist, working with people in neurorehabilitation. This enabled me to hone my communication skills and develop great empathy practices.'
+export const present = 'I aim to bring a fully rounded skill set to each project, and approach everthing with integrity and wholeheartedness.'
